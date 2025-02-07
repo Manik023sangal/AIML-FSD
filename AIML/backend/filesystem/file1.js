ff491eba3a4d2466fc9582cebadc1381b132398f
Manik023sangal <maniksangal46@gmail.com>
2025-02-07 09:43:05 +0530

    updated work 7/2/25

--- AIML/backend/filesystem/file1.js
+++ AIML/backend/filesystem/file1.js
@@ -1,5 +1,23 @@
+// const fs = require('fs');
+// // const fs = require('node:fs');
+// // const data = fs.readFileSync("./data.txt", "utf-8");//encoding scheme like encoding:"utf-8"
+// // console.log(data); //for ascii
+// // console.log(data.toString());
+
+// fs.writeFileSync("./data1.txt", 'Hey This is my new work', 'utf-8');
+
+// const fs = require('fs');
+// fs.writeFileSync("./data2.txt", 'Hey This is my new work', 'utf-8');
+// fs.appendFileSync("./data1.txt", ' Work form another file', 'utf-8');
+// fs.renameSync('./data2.txt', './data3.txt');
+
+// fs.unlinkSync('./data3.txt'); // delete
+
 const fs = require('fs');
-// const fs = require('node:fs');
-const data = fs.readFileSync("./data.txt", "utf-8");//encoding scheme like encoding:"utf-8"
-console.log(data); //for ascii
-// console.log(data.toString());
+const data = fs.readFileSync("./data.txt", "utf-8");
+console.log(data);
+
+if(data.match("H")){
+  const newdata = data.replace("H", "ABES");
+  fs.writeFileSync('./data.txt', newdata, 'utf-8');  
+}
